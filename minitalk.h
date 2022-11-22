7a217c28b70975053b74d32aead15d98de8cecbe
tinitiuset <tinitiuset@gmail.com>
2022-11-22 12:49:53 +0100

    Created Minitalk header file

---
+++ minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 12:31:47 by mvalient          #+#    #+#             */
+/*   Updated: 2022/11/22 12:32:35 by mvalient         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./libft_extra/ft_printf/ft_printf.h"
+# include "./libft_extra/get_next_line/get_next_line.h"
+# include "./libft_extra/libft/libft.h"
+
+#endif
