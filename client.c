f6a68fede82e3bac396f85bb059671a117ec1086
tinitiuset <tinitiuset@gmail.com>
2022-11-23 23:12:41 +0100

    Client parameter handling done

--- client.c
+++ client.c
@@ -6,8 +6,21 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:52:56 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/23 22:52:56 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/23 23:12:08 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	int	target_pid;
+
+	if (argc == 3)
+	{
+		target_pid = ft_atoi(argv[1]);
+	}
+	else
+		ft_printf("Error: You should input PID and MESSAGE as parameters");
+	return (0);
+}
