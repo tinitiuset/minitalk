463e160fd617dfc678efd7e70664626202b0d49d
tinitiuset <tinitiuset@gmail.com>
2022-11-23 23:21:12 +0100

    Created macro definitions

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 12:31:47 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/22 12:32:35 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/23 23:20:05 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include "./libft_extra/ft_printf/ft_printf.h"
 # include "./libft_extra/get_next_line/get_next_line.h"
 # include "./libft_extra/libft/libft.h"
+
+# define FALSE SIGUSR1
+# define TRUE SIGUSR2
 
 #endif
