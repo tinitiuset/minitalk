441850955baf4925ab5de521810ade20b1c91b8b
tinitiuset <tinitiuset@gmail.com>
2022-11-24 11:19:31 +0100

    Server prints message in binary

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 12:31:47 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/23 23:20:05 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/24 10:36:06 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "./libft_extra/ft_printf/ft_printf.h"
 # include "./libft_extra/get_next_line/get_next_line.h"
 # include "./libft_extra/libft/libft.h"
+# include "signal.h"
 
 # define FALSE SIGUSR1
 # define TRUE SIGUSR2
--- server.c
+++ server.c
@@ -6,14 +6,28 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:48:42 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/23 22:59:08 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:16:46 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	ft_handle_false(void)
+{
+	ft_printf("0");
+}
+
+void	ft_handle_true(void)
+{
+	ft_printf("1");
+}
+
 int	main(void)
 {
-	ft_printf("PID: %d", getpid())
+	ft_printf("PID: %d\n", getpid());
+	signal(TRUE, ft_handle_true);
+	signal(FALSE, ft_handle_false);
+	while (1)
+		pause();
 	return (0);
 }
