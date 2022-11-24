07eec2f6b77e1f7402d85694e395c78244bdf13a
tinitiuset <tinitiuset@gmail.com>
2022-11-24 13:02:44 +0100

    Server now displays each char in a line as binary

--- server.c
+++ server.c
@@ -6,27 +6,38 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:48:42 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/24 11:16:46 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/24 12:15:54 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_handle_false(void)
+static void	ft_handle(int signum)
 {
-	ft_printf("0");
-}
+	static int	i;
 
-void	ft_handle_true(void)
-{
-	ft_printf("1");
+	if (signum == 10)
+	{
+		ft_printf("0");
+		i++;
+	}
+	else if (signum == 12)
+	{
+		ft_printf("1");
+		i++;
+	}
+	if (i == 8)
+	{
+		ft_printf("\n");
+		i = 0;
+	}
 }
 
 int	main(void)
 {
 	ft_printf("PID: %d\n", getpid());
-	signal(TRUE, ft_handle_true);
-	signal(FALSE, ft_handle_false);
+	signal(TRUE, ft_handle);
+	signal(FALSE, ft_handle);
 	while (1)
 		pause();
 	return (0);
