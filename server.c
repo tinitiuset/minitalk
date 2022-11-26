da285d6d2afe3408cdc82f8a11f2eb030d14f5f3
tinitiuset <tinitiuset@gmail.com>
2022-11-26 22:45:27 +0100

    Added Error handling to Server

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:48:42 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/25 20:47:03 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/26 22:44:47 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ int	main(void)
 	ft_printf("PID: %d\n", getpid());
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_handler = ft_handle;
-	sigaction(TRUE, &sa, NULL);
-	sigaction(FALSE, &sa, NULL);
+	if (sigaction(TRUE, &sa, NULL) == -1)
+		write(1, "Error\n", 6);
+	if (sigaction(FALSE, &sa, NULL) == -1)
+		write(1, "Error\n", 6);
 	while (1)
 		pause();
 	return (0);
