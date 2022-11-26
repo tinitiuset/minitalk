5fdabf67d88ed1145b8e70071fd4aa8d6f1977bd
tinitiuset <tinitiuset@gmail.com>
2022-11-26 23:16:58 +0100

    Added support in client for confirmation Signals

--- client.c
+++ client.c
@@ -6,13 +6,21 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:52:56 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/25 20:53:13 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/26 23:08:15 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_send_message(int target, char *message)
+static void	ft_handle(int signum, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	if (signum == TRUE)
+		write(1, "Signal sent correctly\n", 22);
+}
+
+static void	ft_send_message(int target, char *message)
 {
 	int	i;
 
@@ -34,10 +42,14 @@ void	ft_send_message(int target, char *message)
 
 int	main(int argc, char **argv)
 {
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ft_handle;
+	if (sigaction(TRUE, &sa, NULL) == -1)
+		write(1, "Error\n", 6);
 	if (argc == 3)
-	{
 		ft_send_message(ft_atoi(argv[1]), argv[2]);
-	}
 	else
 		ft_printf("Error: You should input PID and MESSAGE as parameters");
 	return (0);
